1e1bee37f84ae3385da632771367599bf51632a3
sudhansu <dashssd@gmail.com>
2019-02-23 20:51:24 +0530

    do while loop is added
    Change-Id: Iae80deeb7542cef5ac248f8be8590b4aecf8e463
    Signed-off-by: sudhansu <dashssd@gmail.com>

--- Test2.java
+++ Test2.java
@@ -3,6 +3,12 @@ public class Test2
 {
 public static void main(String args[])
 {
+	int i=10;
+	do
+	{
+		System.out.println("i value"+i*2);
+		
+	}while(i!=0);
 System.out.println(" i am here tohelp you");
 }
 
